3f5329fc00b017c41e573f7bb56778066fb2c291
Dawid Szabłowski <dawidszab@wp.pl>
2022-03-05 21:39:37 +0100

    Task 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, animals!");
+
+        Animal dog = new Animal("canis","Szarik");
+
+//        Animal dog = new Animal();
+//        dog.species = "canis";
+//        dog.alive = true;
+//        dog.age = 7;
+//        dog.weight = 24.0;
+//        dog.name = "Burek";
+        System.out.println(dog.name);
+        System.out.println("doggo w " + dog.getWeight());
+        dog.feed();
+        dog.feed();
+        dog.feed();
+        System.out.println("doggo w " + dog.getWeight());
     }
 }
